4bd53be6bb08489f508761be69d615260ebc7000
Poorvi Agrawal <agrawalpoorvi75@gmail.com>
2026-01-02 05:16:56 +0000

    Try fixing this error: `Runtime Error: Module [project]/node_modules/@so

--- src/components/wallet-provider.tsx
+++ src/components/wallet-provider.tsx
@@ -8,7 +8,7 @@ import { clusterApiUrl } from '@solana/web3.js';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-unsafe-burner';
 
 // Default styles that can be overridden by your app
-require('@solana/wallet-adapter-react-ui/styles.css');
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export function WalletProvider({ children }: { children: React.ReactNode }) {
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
